e6d22f959e90fcd1ee75f23c72ac14fc61b7a210
alifirozjaei <ali.firozjaeii@gmail.com>
2024-02-20 15:00:58 +0330

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 1 on 12/1/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
